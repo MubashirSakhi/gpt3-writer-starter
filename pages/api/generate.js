09b38da5afc87f3029989e52328de2cd6c22bf33
mubashirsakhi <mubashirsakhi@gmail.com>
2022-12-27 23:48:45 +0500

    add some changes to the prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -18,31 +18,31 @@ const generateAction = async (req, res) => {
     temperature: 0.8,
     max_tokens: 250,
   });
-  
+
   const basePromptOutput = baseCompletion.data.choices.pop();
 
   // I build Prompt #2.
-  const secondPrompt = 
-  `
+  const secondPrompt =
+    `
   ${celebrityPrompts[req.body.celebId].prompts[1]}
 
   Idea: ${req.body.userInput}
 
   pitch: ${basePromptOutput.text}
-
+===BEGIN===
   Slides:
   `
-  
+
   // I call the OpenAI API a second time with Prompt #2
   const secondPromptCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `${secondPrompt}`,
     // I set a higher temperature for this one. Up to you!
     temperature: 0.85,
-		// I also increase max_tokens.
+    // I also increase max_tokens.
     max_tokens: 1250,
   });
-  
+
   // Get the output
   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
 
--- pages/api/prompts.js
+++ pages/api/prompts.js
@@ -4,7 +4,7 @@ const celebrityPrompts = [
         "prompts":[
             `Write a startup pitch in the style of Samuel L. Jackson for the idea below. Keeping in mind the idea is pitch to pre seed investor to raise first round of money for the startup.
             Idea:`,
-            `Take the pitch and idea of the startup below and generate slides for mission, pain points,solution, revenue model. Make it feel like a story. Don't just list the points. Go deep into each one and add data points too. Explain why. Include Samuel Jackson's dialogue in the last slide.`
+            `Take the pitch and idea of the startup below and generate slides for the pain points, solution, competition, and revenue model. Make it feel like a story. Don't just list the points. Go deep into each one and add data points too. Explain why. Include Samuel Jackson's dialogue in the last slide.`
         ]
     },
     {
@@ -12,7 +12,7 @@ const celebrityPrompts = [
         "prompts":[
             `Write a startup pitch in the style of Kevin Hart for the idea below. Keeping in mind the idea is pitch to pre seed investor to raise first round of money for the startup.
             Idea:`,
-            `Take the pitch and idea of the startup below and generate slides for mission, pain points,solution, revenue model. Make it feel like a story. Don't just list the points. Go deep into each one and add data points too. Explain why. Include Kevin Hart's humor in the last slide.`
+            `Take the pitch and idea of the startup below and generate slides the pain points, solution, competition, and revenue model. Make it feel like a story. Don't just list the points. Go deep into each one and add data points too. Explain why. Include Kevin Hart's humor in the last slide.`
         
         ]
     },
@@ -21,7 +21,7 @@ const celebrityPrompts = [
         "prompts":[
             `Write a startup pitch in the style of Scarlett Johanssan for the idea below. Keeping in mind the idea is pitch to pre seed investor to raise first round of money for the startup.
             Idea:`,
-            `Take the pitch and idea of the startup below and generate slides for mission, pain points,solution, revenue model. Make it feel like a story. Don't just list the points. Go deep into each one and add data points too. Explain why. Include Scarlett Johansson's dialogue in the last slide.`
+            `Take the pitch and idea of the startup below and generate slides for the pain points, solution, competition, and revenue model. Make it feel like a story. Don't just list the points. Go deep into each one and add data points too. Explain why. Include Scarlett Johansson's dialogue in the last slide.`
         
         ]
     }
