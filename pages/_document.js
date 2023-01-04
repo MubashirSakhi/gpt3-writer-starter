18a10f40ee36cafdeaea13e2655ef7af04e7b94c
mubashirsakhi <mubashirsakhi@gmail.com>
2023-01-04 13:16:11 +0500

    meta image and description changed

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Pitch Wise" key="title"/>
+        <meta property="og:description" content="Build with Dedication and cool vibes" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://res.cloudinary.com/dusjh0hzg/image/upload/v1672820088/pitch_wise_og_q5l52u.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
